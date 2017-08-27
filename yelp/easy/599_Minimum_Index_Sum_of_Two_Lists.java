d87be7762de3dc3011e0e80399d43693567ebfaa
chengdol <chengdol@usc.edu>
2017-08-26 19:35:21 -0700

    yelp 599

---
+++ yelp/easy/599_Minimum_Index_Sum_of_Two_Lists.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        // map string and index
+        for (int i = 0; i < list1.length; i++)
+        {
+            map.put(list1[i], i);
+        }
+
+        int minSum = Integer.MAX_VALUE;
+        List<String> res = new ArrayList<>();
+
+        for (int i = 0; i < list2.length; i++)
+        {
+            // find common interest
+            if (map.containsKey(list2[i]))
+            {
+                int curSum = map.get(list2[i]) + i;
+                // update result list
+                if (minSum > curSum)
+                {
+                    minSum = curSum;
+                    // here also can new ArrayList<>();
+
+                    res.clear();
+                    res.add(list2[i]);
+                }
+                // append new one
+                else if (minSum == curSum)
+                {
+                    res.add(list2[i]);
+                }
+            }
+        }
+
+        // list to array
+        return res.toArray(new String[0]);
+
+    }
+}
