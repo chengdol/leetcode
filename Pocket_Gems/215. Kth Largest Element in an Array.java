062567dabc48213acf1046a027593e1310a1a127
chengdol <chengdol@usc.edu>
2017-10-07 17:08:24 -0700

    pg 215

--- Pocket_Gems/215. Kth Largest Element in an Array.java
+++ Pocket_Gems/215. Kth Largest Element in an Array.java
@@ -24,75 +24,78 @@ class Solution {
     }
 }
 
-// quick selection
-// shuffle the array first
+// qs
 class Solution {
-    public int findKthLargest(int[] nums, int k)
-    {
-        // random shuffle array
+    // 典型的quick select
+    public int findKthLargest(int[] nums, int k) {
+
+        // shuffle the array
         shuffle(nums);
+        // quick selection alg
+        int t = nums.length - k;
         int lo = 0;
         int hi = nums.length - 1;
-        // 这里转换一下
-        k = nums.length - k;
-        while (hi > lo)
+        // at least one element
+        while (lo < hi)
         {
-            int t = partition(nums, lo, hi);
-            if (t < k) { lo = t + 1; }
-            else if (t > k) { hi = t - 1; }
+            int r = quickSelect(nums, lo, hi);
+            if (r == t) { return nums[r]; }
+            else if (r > t)
+            {
+                hi = r - 1;
+            }
             else
             {
-                // find it
-                break;
+                lo = r + 1;
             }
         }
-        // 只有一个元素的情况
-        return nums[k];
-    }
-
-    // quick sort key part!
-    private int partition(int[] nums, int lo, int hi)
-    {
-        // lo 就是基准
-        int i = lo;
-        // 后面用的--j
-        int j = hi + 1;
-
-        while (true)
-        {
-            while (nums[++i] < nums[lo])
-            {
-                if (i == hi) { break; }
-            }
-            while (nums[--j] > nums[lo])
-            {
-                if (j <= lo) { break; }
-            }
-
-            if (j <= i) { break; }
-            swap(nums, i, j);
-        }
-        // 记得最后swap
-        swap(nums, lo, j);
-        return j;
+        // only one element
+        return nums[t];
     }
 
     private void shuffle(int[] nums)
     {
-        // start from 0
         for (int i = 0; i < nums.length; i++)
         {
-            // Math.random -> [0, 1)
             int j = i + (int)(Math.random() * (nums.length - i));
+            swap(nums, j, i);
+        }
+    }
+
+    private int quickSelect(int[] nums, int lo, int hi)
+    {
+        if (lo >= hi) { return -1; }
+
+        int i = lo;
+        int j = hi + 1;
+        while (true)
+        {
+            // 这里不用关系出边界，因为数组至少有2个元素了
+            while (nums[++i] < nums[lo])
+            {
+                if (i == hi) { break; }
+            }
+
+            while (nums[--j] > nums[lo]);
+            {
+                if (j == lo) { break; }
+            }
+
+            // cross就break;
+            if (j <= i) { break; }
+            // 否则swap
             swap(nums, i, j);
         }
+        // swap lo 与 j
+        swap(nums, lo, j);
+        return j;
     }
 
     private void swap(int[] nums, int i, int j)
     {
-        // no need to use xor swap
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
     }
+
 }
